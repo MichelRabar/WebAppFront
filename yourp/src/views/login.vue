61df1e4470aecb67d6e67f31dfb3ca009ad2aa0f
MichelRabar <rabarmichel@gmail.com>
2024-01-26 23:46:31 +0100

    comm

---
+++ yourp/src/views/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Login</h2>
+      <form @submit.prevent="loginUser">
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" required />
+        
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required />
+  
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      loginUser() {
+        // Ovdje dodajte logiku za prijavu korisnika
+        console.log('Prijavljen korisnik:', this.username);
+      }
+    }
+  };
+  </script>
+  
---
+++ yourp/src/views/signup.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Sign Up</h2>
+      <form @submit.prevent="signupUser">
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" required />
+        
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required />
+  
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      signupUser() {
+        // Ovdje dodajte logiku za registraciju korisnika
+        console.log('Registriran korisnik:', this.username);
+      }
+    }
+  };
+  </script>
+  
